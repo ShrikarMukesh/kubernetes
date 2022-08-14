3c25bcf54b6645bdc10e66e8c09c2a881884d49b
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2022-08-14 19:20:42 +0530

    Practiced config maps 1

---
+++ Kubernetes/varAndArgs/codes/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] as) throws InterruptedException {
+        System.out.println("Hello Docker cmds and variables");
+        System.out.println("I Am Hello.java class");
+        for(String str: as ){
+            System.out.println(str);
+        }
+        for (int i =1;i <=10; i++){
+            Thread.sleep(5000);
+        }
+    }
+}
